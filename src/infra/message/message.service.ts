4e096d94233e25bb19c6f17a59f1f2bfbbea6173
Minh Duc <mrduc0109@gmail.com>
2021-08-24 09:50:18 +0700

    fix(app): interfaces - remove unused codes

--- src/infra/message/message.service.ts
+++ src/infra/message/message.service.ts
@@ -2,16 +2,14 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Message, MessageDocument } from '@src/infra/database/schema';
-// import { CreateLogPayload } from '@src/interfaces/shared/log';
-// import AppConfig from '@src/config/configuration';
-// import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class MessageService {
   constructor(
-    @InjectModel(Message.name) private MessageModel: Model<MessageDocument>, // @Inject(AppConfig.KEY) // private readonly appConfig: ConfigType<typeof AppConfig>,
+    @InjectModel(Message.name) private MessageModel: Model<MessageDocument>,
   ) {}
 
+  // TODO: add type of filter
   async list(filter?: any): Promise<any> {
     return this.MessageModel.find({});
   }
--- src/interfaces/http/rest/api_v1/message/message.controller.ts
+++ src/interfaces/http/rest/api_v1/message/message.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Post, Get, Query, Body } from '@nestjs/common';
 import { MessageService } from '@src/infra/message/message.service';
-// import { Query7DigitalPayload } from '@src/interfaces/shared/proxy.dto';
 
 @Controller('messages')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
   @Get()
-  getStream7Digital(@Query() query: any) {
+  getMessages(@Query() query: any) {
     return this.messageService.list(query);
   }
 
